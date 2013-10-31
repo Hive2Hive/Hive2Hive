06c0608116abc2795cf570f84acf3e6824e5bc59
Nico Rutishauser <nr@hotmail.ch>
2013-10-31 17:59:01 +0100

    Added cleanup for storage memory

--- org.hive2hive.core/src/org/hive2hive/core/H2HConstants.java
+++ org.hive2hive.core/src/org/hive2hive/core/H2HConstants.java
@@ -8,6 +8,10 @@ public interface H2HConstants {
 	// configurations for network messages
 	public static final int MAX_MESSAGE_SENDING = 5;
 
+	// maximal numbers of versions kept in the DHT (see versionKey)
+	public static final int MAX_VERSIONS_HISTORY = 5;
+	public static final long MIN_VERSIONS_AGE_BEFORE_REMOVAL_MS = 5 * 60 * 1000; // 5mins
+
 	// DHT content keys - these are used to distinguish the different types data
 	// stored for a given key
 	public static final String USER_PROFILE = "USER_PROFILE";
--- org.hive2hive.core/src/org/hive2hive/core/network/H2HStorageMemory.java
+++ org.hive2hive.core/src/org/hive2hive/core/network/H2HStorageMemory.java
@@ -1,13 +1,19 @@
 package org.hive2hive.core.network;
 
 import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import net.tomp2p.peers.Number160;
 import net.tomp2p.storage.Data;
 import net.tomp2p.storage.StorageMemory;
 
+import org.hive2hive.core.H2HConstants;
 import org.hive2hive.core.log.H2HLogger;
 import org.hive2hive.core.log.H2HLoggerFactory;
 
@@ -46,9 +52,8 @@ public class H2HStorageMemory extends StorageMemory {
 					domainProtection);
 
 			// After adding the content to the memory, old versions should be cleaned up. How many old
-			// versions we keep could probably be parameterized. I (Nico) would recommend to keep at least 2
-			// or 3 versions, thus we can recognize concurrent modification better (else, the 'previous
-			// version' hash is always wrong).
+			// versions we keep can be parameterized in the constants.
+			cleanupVersions(locationKey, domainKey, contentKey, versionKey);
 		}
 
 		logger.debug("Finished verification (" + status + ") of locationKey: " + locationKey + " domainKey: "
@@ -64,46 +69,37 @@ public class H2HStorageMemory extends StorageMemory {
 		Map<Number160, Data> history = new HashMap<Number160, Data>();
 
 		/** 1. if version is null and no history yet, it is the first entry here **/
-		if (history.isEmpty() && versionKey == null) {
-			logger.debug("First version of a content is added");
-			return PutStatus.OK;
+		if (versionKey == null) {
+			// no version key given
+			if (history.isEmpty()) {
+				logger.debug("First version of a content is added");
+				return PutStatus.OK;
+			} else {
+				logger.error("No version key given but history is not empty");
+				return PutStatus.VERSION_CONFLICT;
+			}
 		}
 
 		/** 2. check if previous exists **/
 		VersionKey newVersionKey = new VersionKey(versionKey);
-		Data previousVersion = null;
-		for (Number160 key : history.keySet()) {
-			Data data = history.get(key);
-			// TODO verify why 'hash()' returns only 160bit instad of 96bit number
-			// TODO how to compare two hashes?
-			if (data.hash().equals(newVersionKey.getPreviousHash())) {
-				previousVersion = data;
-				break;
-			}
-		}
+		VersionKey parentKey = getParent(newVersionKey, history);
 
-		if (previousVersion == null) {
+		if (parentKey == null) {
 			// previous version not found
 			logger.error("Previous version with key " + newVersionKey.getPreviousHash() + " not found");
 			return PutStatus.VERSION_CONFLICT;
 		}
 
 		/** 3. Check if previous version is latest one **/
-		VersionKey latestVersionKey = null;
-		for (Number160 key : history.keySet()) {
-			VersionKey version = new VersionKey(key);
-			if (latestVersionKey == null || latestVersionKey.getTimestamp() < version.getTimestamp()) {
-				latestVersionKey = version;
-			}
-		}
-		Data latestVersion = history.get(latestVersionKey.getVersionKey());
-
-		if (latestVersion == null) {
+		Data parentData = history.get(parentKey.getVersionKey());
+		VersionKey latestVersionKey = getLatestVersion(history);
+		if (latestVersionKey == null) {
 			logger.error("Latest version not found. This should have never happened");
 			return PutStatus.VERSION_CONFLICT;
 		}
 
-		if (latestVersion == previousVersion) {
+		Data latestVersion = history.get(latestVersionKey.getVersionKey());
+		if (latestVersion.equals(parentData)) {
 			// previous version is the latest one (continue).
 			logger.debug("New content is based on latest version.");
 			return PutStatus.OK;
@@ -112,5 +108,63 @@ public class H2HStorageMemory extends StorageMemory {
 			logger.error("New content does not base on latest version in storage");
 			return PutStatus.VERSION_CONFLICT;
 		}
+	}
+
+	private void cleanupVersions(Number160 locationKey, Number160 domainKey, Number160 contentKey,
+			Number160 versionKey) {
+		// TODO This list is only a stub, get it from the local storage (key = versionKey, value = data)
+		/* Map<Number160, Data> history = get(locationKey, domainKey, contentKey */
+		Map<Number160, Data> history = new HashMap<Number160, Data>();
+		Set<Number160> keys = history.keySet();
+
+		// add them to a list and sort them
+		List<VersionKey> versionKeys = new ArrayList<VersionKey>();
+		for (Number160 key : keys) {
+			versionKeys.add(new VersionKey(key));
+		}
+
+		// TODO check if order is correct such that the oldest ones get removed
+		Collections.sort(versionKeys, new Comparator<VersionKey>() {
+			@Override
+			public int compare(VersionKey o1, VersionKey o2) {
+				return new Long(o1.getTimestamp()).compareTo(o2.getTimestamp());
+			}
+		});
+
+		long now = System.currentTimeMillis();
+		while (versionKeys.size() >= H2HConstants.MAX_VERSIONS_HISTORY) {
+			VersionKey toRemove = versionKeys.get(0);
+			if (toRemove.getTimestamp() + H2HConstants.MIN_VERSIONS_AGE_BEFORE_REMOVAL_MS > now) {
+				// stop removal because oldest version is too 'young'
+				break;
+			} else {
+				remove(locationKey, domainKey, contentKey /* , toRemove.getVersionKey() */);
+			}
+		}
+	}
+
+	private VersionKey getLatestVersion(Map<Number160, Data> history) {
+		VersionKey latestVersionKey = null;
+		for (Number160 key : history.keySet()) {
+			VersionKey version = new VersionKey(key);
+			if (latestVersionKey == null || latestVersionKey.getTimestamp() < version.getTimestamp()) {
+				latestVersionKey = version;
+			}
+		}
+
+		return latestVersionKey;
+	}
+
+	private VersionKey getParent(VersionKey child, Map<Number160, Data> history) {
+		for (Number160 key : history.keySet()) {
+			Data data = history.get(key);
+			// TODO verify why 'hash()' returns only 160bit instad of 96bit number
+			// TODO how to compare two hashes?
+			if (data.hash().equals(child.getPreviousHash())) {
+				return new VersionKey(key);
+			}
+		}
+
+		return null;
 	}
 }
