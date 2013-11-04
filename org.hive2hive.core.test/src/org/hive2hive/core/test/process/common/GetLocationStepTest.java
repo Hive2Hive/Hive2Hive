1ee82aee09b703415d837601cf183b740408cafa
Nico Rutishauser <nr@hotmail.ch>
2013-11-04 16:27:53 +0100

    Add tests for GetLocations and GetUserProfile

---
+++ org.hive2hive.core.test/src/org/hive2hive/core/test/process/common/GetLocationStepTest.java
@@ -0,0 +1,91 @@
+package org.hive2hive.core.test.process.common;
+
+import java.util.List;
+
+import org.hive2hive.core.H2HConstants;
+import org.hive2hive.core.model.Locations;
+import org.hive2hive.core.network.NetworkManager;
+import org.hive2hive.core.process.Process;
+import org.hive2hive.core.process.common.GetLocationsStep;
+import org.hive2hive.core.test.H2HJUnitTest;
+import org.hive2hive.core.test.H2HWaiter;
+import org.hive2hive.core.test.network.NetworkTestUtil;
+import org.hive2hive.core.test.process.TestProcessListener;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Tests the generic step that puts the location into the DHT
+ * 
+ * @author Nico
+ * 
+ */
+public class GetLocationStepTest extends H2HJUnitTest {
+
+	private static List<NetworkManager> network;
+	private static final int networkSize = 2;
+
+	@BeforeClass
+	public static void initTest() throws Exception {
+		testClass = GetLocationStepTest.class;
+		beforeClass();
+	}
+
+	@Override
+	@Before
+	public void beforeMethod() {
+		super.beforeMethod();
+		network = NetworkTestUtil.createNetwork(networkSize);
+	}
+
+	@Test
+	public void testStepSuccessful() throws InterruptedException {
+		NetworkManager putter = network.get(0); // where the process runs
+		NetworkManager proxy = network.get(1); // where the user profile is stored
+
+		// create the needed objects
+		String userId = proxy.getNodeId();
+		Locations newLocations = new Locations(userId);
+		newLocations.addOnlinePeer(putter.getPeerAddress());
+
+		// put the locations to the DHT
+		proxy.putLocal(userId, H2HConstants.USER_LOCATIONS, newLocations);
+
+		// initialize the process and the one and only step to test
+		Process process = new Process(putter) {
+		};
+		GetLocationsStep step = new GetLocationsStep(userId, null);
+		process.setFirstStep(step);
+		TestProcessListener listener = new TestProcessListener();
+		process.addListener(listener);
+		process.start();
+
+		// wait for the process to finish
+		H2HWaiter waiter = new H2HWaiter(20);
+		do {
+			waiter.tickASecond();
+		} while (!listener.hasSucceeded());
+
+		Locations found = step.getLocations();
+
+		// verify if both objects are the same
+		Assert.assertEquals(userId, found.getUserId());
+		Assert.assertEquals(putter.getPeerAddress(), found.getOnlinePeers().get(0).getAddress());
+	}
+
+	@Override
+	@After
+	public void afterMethod() {
+		NetworkTestUtil.shutdownNetwork(network);
+		super.afterMethod();
+	}
+
+	@AfterClass
+	public static void endTest() {
+		afterClass();
+	}
+}
---
+++ org.hive2hive.core.test/src/org/hive2hive/core/test/process/common/GetUserProfileStepTest.java
@@ -0,0 +1,118 @@
+package org.hive2hive.core.test.process.common;
+
+import java.io.IOException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.List;
+
+import javax.crypto.SecretKey;
+
+import net.tomp2p.futures.FuturePut;
+
+import org.bouncycastle.crypto.DataLengthException;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.hive2hive.core.H2HConstants;
+import org.hive2hive.core.model.UserProfile;
+import org.hive2hive.core.network.NetworkManager;
+import org.hive2hive.core.process.Process;
+import org.hive2hive.core.process.common.GetUserProfileStep;
+import org.hive2hive.core.security.EncryptedNetworkContent;
+import org.hive2hive.core.security.EncryptionUtil;
+import org.hive2hive.core.security.EncryptionUtil.AES_KEYLENGTH;
+import org.hive2hive.core.security.EncryptionUtil.RSA_KEYLENGTH;
+import org.hive2hive.core.security.H2HEncryptionUtil;
+import org.hive2hive.core.security.PasswordUtil;
+import org.hive2hive.core.security.UserPassword;
+import org.hive2hive.core.test.H2HJUnitTest;
+import org.hive2hive.core.test.H2HWaiter;
+import org.hive2hive.core.test.network.NetworkTestUtil;
+import org.hive2hive.core.test.process.TestProcessListener;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Tests the generic step that puts the user profile into the DHT
+ * 
+ * @author Nico
+ * 
+ */
+public class GetUserProfileStepTest extends H2HJUnitTest {
+
+	private static List<NetworkManager> network;
+	private static final int networkSize = 2;
+
+	@BeforeClass
+	public static void initTest() throws Exception {
+		testClass = GetUserProfileStepTest.class;
+		beforeClass();
+	}
+
+	@Override
+	@Before
+	public void beforeMethod() {
+		super.beforeMethod();
+		network = NetworkTestUtil.createNetwork(networkSize);
+	}
+
+	@Test
+	public void testStepSuccessful() throws InterruptedException, InvalidKeySpecException,
+			DataLengthException, IllegalStateException, InvalidCipherTextException, ClassNotFoundException,
+			IOException {
+		NetworkManager putter = network.get(0); // where the process runs
+		NetworkManager client = network.get(1); // where the user profile is stored
+
+		// create the needed objects
+		String userId = NetworkTestUtil.randomString();
+		String password = NetworkTestUtil.randomString();
+		String pin = generateRandomString(6);
+		UserPassword userPassword = new UserPassword(password, pin);
+		UserProfile testProfile = new UserProfile(userId,
+				EncryptionUtil.generateRSAKeyPair(RSA_KEYLENGTH.BIT_1024),
+				EncryptionUtil.generateRSAKeyPair(RSA_KEYLENGTH.BIT_1024));
+
+		// add them already to the DHT
+		SecretKey encryptionKeys = PasswordUtil.generateAESKeyFromPassword(userPassword,
+				AES_KEYLENGTH.BIT_256);
+		EncryptedNetworkContent encrypted = H2HEncryptionUtil.encryptAES(testProfile, encryptionKeys);
+		FuturePut putGlobal = putter.putGlobal(testProfile.getLocationKey(userPassword),
+				H2HConstants.USER_PROFILE, encrypted);
+		putGlobal.awaitUninterruptibly();
+		putGlobal.getFutureRequests().awaitUninterruptibly();
+
+		// initialize the process and the one and only step to test
+		Process process = new Process(putter) {
+		};
+		GetUserProfileStep step = new GetUserProfileStep(userId, userPassword, null);
+		process.setFirstStep(step);
+		TestProcessListener listener = new TestProcessListener();
+		process.addListener(listener);
+		process.start();
+
+		// wait for the process to finish
+		H2HWaiter waiter = new H2HWaiter(20);
+		do {
+			waiter.tickASecond();
+		} while (!listener.hasSucceeded());
+
+		UserProfile profile = step.getUserProfile();
+
+		// verify if both objects are the same
+		Assert.assertEquals(userId, profile.getUserId());
+		Assert.assertEquals(testProfile.getTimestamp(), profile.getTimestamp());
+	}
+
+	@Override
+	@After
+	public void afterMethod() {
+		NetworkTestUtil.shutdownNetwork(network);
+		super.afterMethod();
+	}
+
+	@AfterClass
+	public static void endTest() {
+		afterClass();
+	}
+}
