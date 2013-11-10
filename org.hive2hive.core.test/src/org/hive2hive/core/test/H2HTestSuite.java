a22a5b4401476a12c995ed41e0b35ab9894262ca
Nico Rutishauser <nr@hotmail.ch>
2013-11-10 19:48:41 +0100

    Add Upload & Download to TestSuite

--- org.hive2hive.core.test/src/org/hive2hive/core/test/H2HTestSuite.java
+++ org.hive2hive.core.test/src/org/hive2hive/core/test/H2HTestSuite.java
@@ -17,8 +17,10 @@ import org.hive2hive.core.test.process.common.put.PutLocationStepTest;
 import org.hive2hive.core.test.process.common.put.PutProcessStepTest;
 import org.hive2hive.core.test.process.common.put.PutUserProfileStepTest;
 import org.hive2hive.core.test.process.common.remove.RemoveProcessStepTest;
+import org.hive2hive.core.test.process.download.DownloadFileTest;
 import org.hive2hive.core.test.process.manager.ProcessManagerTest;
 import org.hive2hive.core.test.process.register.RegisterTest;
+import org.hive2hive.core.test.process.upload.UploadFileTest;
 import org.hive2hive.core.test.tomp2p.ReplicationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -41,18 +43,24 @@ import org.junit.runners.Suite.SuiteClasses;
 		BaseRequestMessageTest.class,
 
 		// Processes
-		ProcessTest.class, ProcessManagerTest.class, GetProcessStepTest.class,
-		PutProcessStepTest.class, RemoveProcessStepTest.class,
+		ProcessTest.class, ProcessManagerTest.class, GetProcessStepTest.class, PutProcessStepTest.class,
+		RemoveProcessStepTest.class,
 
 		// Process: Common steps
 		PutUserProfileStepTest.class, PutLocationStepTest.class, GetUserProfileStepTest.class,
 		GetLocationStepTest.class,
-		
+
 		// Process: Common steps, Messages
 		BaseMessageProcessStepTest.class, BaseDirectMessageProcessStepTest.class,
 
 		// Process: Register
 		RegisterTest.class,
+
+		// Process: Upload
+		UploadFileTest.class,
+
+		// Process: Download
+		DownloadFileTest.class,
 
 		// Files
 		FileManagerTest.class,
