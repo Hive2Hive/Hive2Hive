967c103cae672e438543eb33569a6e72d4a552a2
Christian <Christian@JARVIS.home>
2013-11-11 12:39:01 +0100

    GetNextFromQueueMessageTest (not working yet)

--- org.hive2hive.core.test/src/org/hive2hive/core/test/network/messages/UserMessageTests.java
+++ org.hive2hive.core.test/src/org/hive2hive/core/test/network/messages/UserMessageTests.java
@@ -1,20 +1,31 @@
 package org.hive2hive.core.test.network.messages;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.util.List;
 import java.util.Random;
 
+import org.hive2hive.core.H2HConstants;
+import org.hive2hive.core.model.UserMessageQueue;
+import org.hive2hive.core.model.UserProfile;
 import org.hive2hive.core.network.NetworkManager;
+import org.hive2hive.core.network.data.NetworkContent;
 import org.hive2hive.core.network.messages.IBaseMessageListener;
 import org.hive2hive.core.network.messages.direct.response.IResponseCallBackHandler;
 import org.hive2hive.core.network.messages.direct.response.ResponseMessage;
 import org.hive2hive.core.network.messages.futures.FutureDirectListener;
 import org.hive2hive.core.network.messages.usermessages.ContactPeerUserMessage;
+import org.hive2hive.core.network.messages.usermessages.GetNextFromQueueMessage;
+import org.hive2hive.core.network.messages.usermessages.GetNextFromQueueMessage.NextFromQueueResponse;
+import org.hive2hive.core.network.messages.usermessages.UserMessage;
+import org.hive2hive.core.security.UserCredentials;
 import org.hive2hive.core.test.H2HJUnitTest;
 import org.hive2hive.core.test.H2HWaiter;
 import org.hive2hive.core.test.network.NetworkTestUtil;
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -50,6 +61,8 @@ public class UserMessageTests extends H2HJUnitTest {
 	}
 
 	private static boolean contactPeerUserMessageHandled;
+	private static boolean getNextFromQueueMessageHandled;
+
 	@Test
 	public void ContactPeerUserMessageTest() {
 
@@ -66,9 +79,11 @@ public class UserMessageTests extends H2HJUnitTest {
 			public void handleResponseMessage(ResponseMessage responseMessage) {
 				// handle callback
 				contactPeerUserMessageHandled = true;
-				String responseEvidence = (String) responseMessage.getContent();
-				Assert.assertTrue(evidence.equals(responseEvidence));
 				logger.debug("ContactPeerUserMessage got handled.");
+
+				String responseEvidence = (String) responseMessage.getContent();
+				assertNotNull(responseEvidence);
+				assertTrue(evidence.equals(responseEvidence));
 			}
 		});
 
@@ -80,10 +95,10 @@ public class UserMessageTests extends H2HJUnitTest {
 
 			@Override
 			public void onFailure() {
-				Assert.fail("The sending of the message failed.");
+				fail("The sending of the message failed.");
 			}
 		}, message, node1));
-		
+
 		// wait for callback handling
 		H2HWaiter waiter = new H2HWaiter(10);
 		do {
@@ -91,7 +106,75 @@ public class UserMessageTests extends H2HJUnitTest {
 		} while (!contactPeerUserMessageHandled);
 	}
 
+	@Test
 	public void GetNextFromQueueMessageTest() {
 
+		// TODO the request to the proxy actually should not happen this way, since the PeerAddress is unknown
+
+		// select two random nodes
+		NetworkManager user = network.get(random.nextInt(network.size()));
+		NetworkManager proxy = network.get(random.nextInt(network.size()));
+
+		// define a random user
+		UserCredentials credentials = NetworkTestUtil.generateRandomCredentials();
+		String userLocationKey = "mama";//UserProfile.getLocationKey(credentials);
+
+		// prepare and put a sample UserMessageQueue for the user from node2
+		UserMessageQueue umq = new UserMessageQueue(credentials.getUserId());
+		for (int i = 0; i < 10; i++) {
+			umq.getMessageQueue().add(new UserMessage(user.getPeerAddress()) {
+
+				private static final long serialVersionUID = -2306638328974008903L;
+
+				@Override
+				public void run() {
+					// do nothing
+				}
+			});
+		}
+		proxy.putLocal(userLocationKey, H2HConstants.USER_MESSAGE_QUEUE_KEY, umq);
+
+		// wait for proxy to put
+		H2HWaiter putWaiter = new H2HWaiter(60);
+		NetworkContent tmp = null;
+		do {
+			putWaiter.tickASecond();
+			tmp = proxy.getLocal(userLocationKey, H2HConstants.USER_MESSAGE_QUEUE_KEY);
+		} while (tmp == null);
+		
+		// request the UserMessageQueue
+		GetNextFromQueueMessage message = new GetNextFromQueueMessage(user.getPeerAddress(),
+				proxy.getPeerAddress(), userLocationKey);
+		message.setCallBackHandler(new IResponseCallBackHandler() {
+			@Override
+			public void handleResponseMessage(ResponseMessage responseMessage) {
+				// handle callback
+				getNextFromQueueMessageHandled = true;
+				logger.debug("GetNextFromQueueMessage got handled.");
+
+				NextFromQueueResponse response = (NextFromQueueResponse) responseMessage.getContent();
+				assertNotNull(response);
+				assertNotNull(response.getUserMessage());
+				assertNotNull(response.getRemainingCount());
+				assertTrue(response.getRemainingCount() == 9);
+			}
+		});
+
+		user.sendDirect(message).addListener(new FutureDirectListener(new IBaseMessageListener() {
+			@Override
+			public void onSuccess() {
+			}
+
+			@Override
+			public void onFailure() {
+				fail("The sending of the message failed.");
+			}
+		}, message, user));
+
+		// wait for callback handling
+		H2HWaiter waiter = new H2HWaiter(10);
+		do {
+			waiter.tickASecond();
+		} while (!getNextFromQueueMessageHandled);
 	}
 }
--- org.hive2hive.core/src/org/hive2hive/core/network/messages/usermessages/GetNextFromQueueMessage.java
+++ org.hive2hive.core/src/org/hive2hive/core/network/messages/usermessages/GetNextFromQueueMessage.java
@@ -8,10 +8,11 @@ import net.tomp2p.peers.PeerAddress;
 import org.hive2hive.core.H2HConstants;
 import org.hive2hive.core.model.UserMessageQueue;
 
+// TODO this might actually not be a user message
 public class GetNextFromQueueMessage extends RequestUserMessage {
 
 	private static final long serialVersionUID = 580669795666539208L;
-	
+
 	private final String locationKey;
 
 	public GetNextFromQueueMessage(PeerAddress senderAddress, PeerAddress targetAddress, String locationKey) {
@@ -23,24 +24,26 @@ public class GetNextFromQueueMessage extends RequestUserMessage {
 	public synchronized void run() {
 
 		// load the next user message
-		UserMessageQueue umQueue = (UserMessageQueue)networkManager.getLocal(locationKey, H2HConstants.USER_MESSAGE_QUEUE_KEY);
+		UserMessageQueue umQueue = (UserMessageQueue) networkManager.getLocal(locationKey,
+				H2HConstants.USER_MESSAGE_QUEUE_KEY);
 		Queue<UserMessage> messageQueue = umQueue.getMessageQueue();
-		
+
 		// response object
-		NextFromQueueResponse responseContent = new NextFromQueueResponse(messageQueue.poll(), messageQueue.size());
-		
+		NextFromQueueResponse responseContent = new NextFromQueueResponse(messageQueue.poll(),
+				messageQueue.size());
+
 		// send it back in a ResponseMessage
 		sendResponse(createResponse(responseContent));
 	}
-	
+
 	public final class NextFromQueueResponse implements Serializable {
-		
+
 		private static final long serialVersionUID = -2850130135183154805L;
-		
+
 		private final UserMessage userMessage;
 		private final int remainingCount;
-		
-		public NextFromQueueResponse(UserMessage message, int remaining){
+
+		public NextFromQueueResponse(UserMessage message, int remaining) {
 			this.userMessage = message;
 			this.remainingCount = remaining;
 		}
