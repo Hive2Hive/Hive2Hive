b9a93a3fb228fca7ec03290bf14b1086939442c2
Ippes <seppi.gol@gmail.com>
2013-11-21 15:21:08 +0100

    storage memory test. Not working because of bug in TomP2P.

---
+++ org.hive2hive.core.test/src/org/hive2hive/core/test/network/H2HStorageMemoryTest.java
@@ -0,0 +1,262 @@
+package org.hive2hive.core.test.network;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.List;
+import java.util.Random;
+
+import net.tomp2p.futures.FuturePut;
+import net.tomp2p.peers.Number160;
+import net.tomp2p.peers.Number640;
+import net.tomp2p.peers.PeerAddress;
+
+import org.hive2hive.core.network.H2HStorageMemory.PutStatusH2H;
+import org.hive2hive.core.network.NetworkManager;
+import org.hive2hive.core.test.H2HJUnitTest;
+import org.hive2hive.core.test.H2HTestData;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class H2HStorageMemoryTest extends H2HJUnitTest {
+
+	private static List<NetworkManager> network;
+	private static final int networkSize = 10;
+	private static Random random = new Random();
+
+	@BeforeClass
+	public static void initTest() throws Exception {
+		testClass = H2HStorageMemoryTest.class;
+		beforeClass();
+		network = NetworkTestUtil.createNetwork(networkSize);
+	}
+	
+	@Test
+	public void putIntialVersionKeyZeroTest() {
+		NetworkManager node = network.get(random.nextInt(networkSize));
+		String locationKey = node.getNodeId();
+		String contentKey = NetworkTestUtil.randomString();
+
+		H2HTestData data = new H2HTestData(NetworkTestUtil.randomString());
+		data.setVersionKey(Number160.ZERO); // not really necessary, is already default value
+
+		FuturePut futurePut = node.getDataManager().putGlobal(locationKey, contentKey, data);
+		futurePut.awaitUninterruptibly();
+
+		assertFalse(futurePut.getRawResult().isEmpty());
+		for (PeerAddress peerAddress : futurePut.getRawResult().keySet()) {
+			for (Number640 key : futurePut.getRawResult().get(peerAddress).keySet()) {
+				assertEquals(PutStatusH2H.OK,
+						PutStatusH2H.values()[futurePut.getRawResult().get(peerAddress).get(key)]);
+			}
+		}
+	}
+	
+	@Test
+	public void putVersionKeyZeroPreviousVersionKeyZeroTest() {
+		NetworkManager node = network.get(random.nextInt(networkSize));
+		String locationKey = node.getNodeId();
+		String contentKey = NetworkTestUtil.randomString();
+
+		H2HTestData data1 = new H2HTestData(NetworkTestUtil.randomString());
+		data1.setVersionKey(Number160.ZERO); // not really necessary, is already default value
+
+		FuturePut futurePut1 = node.getDataManager().putGlobal(locationKey, contentKey, data1);
+		futurePut1.awaitUninterruptibly();
+
+		H2HTestData data2 = new H2HTestData(NetworkTestUtil.randomString());
+		data2.setVersionKey(Number160.ZERO); // not really necessary, is already default value
+
+		FuturePut futurePut2 = node.getDataManager().putGlobal(locationKey, contentKey, data2);
+		futurePut2.awaitUninterruptibly();
+
+		assertFalse(futurePut2.getRawResult().isEmpty());
+		for (PeerAddress peerAddress : futurePut2.getRawResult().keySet()) {
+			for (Number640 key : futurePut2.getRawResult().get(peerAddress).keySet()) {
+				assertEquals(PutStatusH2H.OK,
+						PutStatusH2H.values()[futurePut2.getRawResult().get(peerAddress).get(key)]);
+			}
+		}
+	}
+	
+	@Test
+	public void putVersionKeyZeroPreviousVersionKeyNotZeroTest() {
+		NetworkManager node = network.get(random.nextInt(networkSize));
+		String locationKey = node.getNodeId();
+		String contentKey = NetworkTestUtil.randomString();
+
+		H2HTestData data1 = new H2HTestData(NetworkTestUtil.randomString());
+		data1.generateVersionKey();
+
+		FuturePut futurePut1 = node.getDataManager().putGlobal(locationKey, contentKey, data1);
+		futurePut1.awaitUninterruptibly();
+
+		H2HTestData data2 = new H2HTestData(NetworkTestUtil.randomString());
+		data2.setVersionKey(Number160.ZERO); // not really necessary, is already default value
+
+		FuturePut futurePut2 = node.getDataManager().putGlobal(locationKey, contentKey, data2);
+		futurePut2.awaitUninterruptibly();
+
+		assertFalse(futurePut2.getRawResult().isEmpty());
+		for (PeerAddress peerAddress : futurePut2.getRawResult().keySet()) {
+			for (Number640 key : futurePut2.getRawResult().get(peerAddress).keySet()) {
+				assertEquals(PutStatusH2H.VERSION_CONFLICT_NO_VERSION_KEY,
+						PutStatusH2H.values()[futurePut2.getRawResult().get(peerAddress).get(key)]);
+			}
+		}
+	}
+
+	@Test
+	public void putInitialTest() {
+		NetworkManager node = network.get(random.nextInt(networkSize));
+		String locationKey = node.getNodeId();
+		String contentKey = NetworkTestUtil.randomString();
+
+		H2HTestData data = new H2HTestData(NetworkTestUtil.randomString());
+		data.generateVersionKey();
+
+		assertNotEquals(Number160.ZERO, data.getVersionKey());
+
+		FuturePut futurePut = node.getDataManager().putGlobal(locationKey, contentKey, data);
+		futurePut.awaitUninterruptibly();
+		futurePut.getFutureRequests().awaitUninterruptibly();
+
+		assertFalse(futurePut.getRawResult().isEmpty());
+		for (PeerAddress peerAddress : futurePut.getRawResult().keySet()) {
+			for (Number640 key : futurePut.getRawResult().get(peerAddress).keySet()) {
+				assertEquals(PutStatusH2H.OK,
+						PutStatusH2H.values()[futurePut.getRawResult().get(peerAddress).get(key)]);
+			}
+		}
+	}
+	
+	@Test
+	public void putNoBasedOnTest() {
+		NetworkManager node = network.get(random.nextInt(networkSize));
+		String locationKey = node.getNodeId();
+		String contentKey = NetworkTestUtil.randomString();
+
+		H2HTestData data1 = new H2HTestData(NetworkTestUtil.randomString());
+		data1.generateVersionKey();
+		data1.setBasedOnKey(Number160.ZERO); // not really necessary, is already default value
+
+		FuturePut futurePut1 = node.getDataManager().putGlobal(locationKey, contentKey, data1);
+		futurePut1.awaitUninterruptibly();
+
+		H2HTestData data2 = new H2HTestData(NetworkTestUtil.randomString());
+		data2.generateVersionKey();
+		data1.setBasedOnKey(Number160.ZERO); // not really necessary, is already default value
+
+		FuturePut futurePut2 = node.getDataManager().putGlobal(locationKey, contentKey, data2);
+		futurePut2.awaitUninterruptibly();
+
+		assertFalse(futurePut2.getRawResult().isEmpty());
+		for (PeerAddress peerAddress : futurePut2.getRawResult().keySet()) {
+			for (Number640 key : futurePut2.getRawResult().get(peerAddress).keySet()) {
+				assertEquals(PutStatusH2H.VERSION_CONFLICT_NO_BASED_ON,
+						PutStatusH2H.values()[futurePut2.getRawResult().get(peerAddress).get(key)]);
+			}
+		}
+	}
+
+	@Test
+	public void putVersionConflictTest() {
+		NetworkManager node = network.get(random.nextInt(networkSize));
+		String locationKey = node.getNodeId();
+		String contentKey = NetworkTestUtil.randomString();
+
+		H2HTestData data1 = new H2HTestData(NetworkTestUtil.randomString());
+		data1.generateVersionKey();
+		data1.setBasedOnKey(Number160.ZERO); // not really necessary, is already default value
+
+		FuturePut futurePut1 = node.getDataManager().putGlobal(locationKey, contentKey, data1);
+		futurePut1.awaitUninterruptibly();
+
+		H2HTestData data2 = new H2HTestData(NetworkTestUtil.randomString());
+		data2.generateVersionKey();
+		
+		H2HTestData data3 = new H2HTestData(NetworkTestUtil.randomString());
+		data3.generateVersionKey();
+		data3.setBasedOnKey(data2.getVersionKey());
+
+		FuturePut futurePut3 = node.getDataManager().putGlobal(locationKey, contentKey, data3);
+		futurePut3.awaitUninterruptibly();
+
+		assertFalse(futurePut3.getRawResult().isEmpty());
+		for (PeerAddress peerAddress : futurePut3.getRawResult().keySet()) {
+			for (Number640 key : futurePut3.getRawResult().get(peerAddress).keySet()) {
+				assertEquals(PutStatusH2H.VERSION_CONFLICT,
+						PutStatusH2H.values()[futurePut3.getRawResult().get(peerAddress).get(key)]);
+			}
+		}
+	}
+	
+	@Test
+	public void putNewVersionTest() {
+		NetworkManager node = network.get(random.nextInt(networkSize));
+		String locationKey = node.getNodeId();
+		String contentKey = NetworkTestUtil.randomString();
+
+		H2HTestData data1 = new H2HTestData(NetworkTestUtil.randomString());
+		data1.generateVersionKey();
+		data1.setBasedOnKey(Number160.ZERO); // not really necessary, is already default value
+
+		FuturePut futurePut1 = node.getDataManager().putGlobal(locationKey, contentKey, data1);
+		futurePut1.awaitUninterruptibly();
+
+		H2HTestData data2 = new H2HTestData(NetworkTestUtil.randomString());
+		data2.generateVersionKey();
+		data2.setBasedOnKey(data1.getVersionKey());
+
+		FuturePut futurePut2 = node.getDataManager().putGlobal(locationKey, contentKey, data2);
+		futurePut2.awaitUninterruptibly();
+
+		assertFalse(futurePut2.getRawResult().isEmpty());
+		for (PeerAddress peerAddress : futurePut2.getRawResult().keySet()) {
+			for (Number640 key : futurePut2.getRawResult().get(peerAddress).keySet()) {
+				assertEquals(PutStatusH2H.OK,
+						PutStatusH2H.values()[futurePut2.getRawResult().get(peerAddress).get(key)]);
+			}
+		}
+	}
+	
+	@Test
+	public void putNewVersionWithOldTimestampTest() {
+		NetworkManager node = network.get(random.nextInt(networkSize));
+		String locationKey = node.getNodeId();
+		String contentKey = NetworkTestUtil.randomString();
+		
+		// fake older time stamp, create data2 before data1
+		H2HTestData data2 = new H2HTestData(NetworkTestUtil.randomString());
+		data2.generateVersionKey();
+
+		H2HTestData data1 = new H2HTestData(NetworkTestUtil.randomString());
+		data1.generateVersionKey();
+
+		data1.setBasedOnKey(Number160.ZERO); // not really necessary, is already default value
+		data2.setBasedOnKey(data1.getVersionKey());
+
+		FuturePut futurePut1 = node.getDataManager().putGlobal(locationKey, contentKey, data1);
+		futurePut1.awaitUninterruptibly();
+
+		FuturePut futurePut2 = node.getDataManager().putGlobal(locationKey, contentKey, data2);
+		futurePut2.awaitUninterruptibly();
+
+		assertFalse(futurePut2.getRawResult().isEmpty());
+		for (PeerAddress peerAddress : futurePut2.getRawResult().keySet()) {
+			for (Number640 key : futurePut2.getRawResult().get(peerAddress).keySet()) {
+				assertEquals(PutStatusH2H.VERSION_CONFLICT_OLD_TIMESTAMP,
+						PutStatusH2H.values()[futurePut2.getRawResult().get(peerAddress).get(key)]);
+			}
+		}
+	}
+
+	@AfterClass
+	public static void cleanAfterClass() {
+		NetworkTestUtil.shutdownNetwork(network);
+		afterClass();
+	}
+
+}
