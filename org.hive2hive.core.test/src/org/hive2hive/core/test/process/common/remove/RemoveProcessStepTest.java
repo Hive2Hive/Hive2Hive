fe3a02063ce0a2161358a11d73421ef401692824
Ippes <seppi.gol@gmail.com>
2013-11-21 19:16:38 +0100

    RemoveProcessStepTest now running

--- org.hive2hive.core.test/src/org/hive2hive/core/test/process/common/remove/RemoveProcessStepTest.java
+++ org.hive2hive.core.test/src/org/hive2hive/core/test/process/common/remove/RemoveProcessStepTest.java
@@ -11,50 +11,41 @@ import net.tomp2p.storage.Data;
 import org.hive2hive.core.network.H2HStorageMemory;
 import org.hive2hive.core.network.NetworkManager;
 import org.hive2hive.core.process.Process;
-import org.hive2hive.core.process.common.remove.BaseRemoveProcessStep;
 import org.hive2hive.core.test.H2HJUnitTest;
 import org.hive2hive.core.test.H2HTestData;
 import org.hive2hive.core.test.H2HWaiter;
 import org.hive2hive.core.test.network.NetworkTestUtil;
 import org.hive2hive.core.test.process.TestProcessListener;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class RemoveProcessStepTest extends H2HJUnitTest {
-	
+
 	private static List<NetworkManager> network;
 	private static final int networkSize = 5;
-	private String contentKey = "a content key";
-	private String data = "some data";
 
 	@BeforeClass
 	public static void initTest() throws Exception {
 		testClass = RemoveProcessStepTest.class;
 		beforeClass();
-	}
-
-	@Override
-	@Before
-	public void beforeMethod() {
-		super.beforeMethod();
 		network = NetworkTestUtil.createNetwork(networkSize);
 	}
 
 	@Test
 	public void testRemoveProcessStep() {
 		String locationKey = network.get(0).getNodeId();
-		H2HTestData testData = new H2HTestData(data);
+		String contentKey = NetworkTestUtil.randomString();
+		H2HTestData testData = new H2HTestData(NetworkTestUtil.randomString());
+
 		// put some data to remove
 		network.get(0).getDataManager().putGlobal(locationKey, locationKey, testData).awaitUninterruptibly();
 
 		// initialize the process and the one and only step to test
 		Process process = new Process(network.get(0)) {
 		};
-		BaseRemoveProcessStep putStep = new BaseRemoveProcessStep(locationKey, contentKey, testData, null);
+		TestRemoveProcessStep putStep = new TestRemoveProcessStep(locationKey, contentKey, testData);
 		process.setNextStep(putStep);
 		TestProcessListener listener = new TestProcessListener();
 		process.addListener(listener);
@@ -65,23 +56,25 @@ public class RemoveProcessStepTest extends H2HJUnitTest {
 		do {
 			waiter.tickASecond();
 		} while (!listener.hasSucceeded());
-		
+
 		assertNull(network.get(0).getDataManager().getLocal(locationKey, contentKey));
 	}
-	
+
 	@Test
 	public void testRemoveProcessStepFailing() {
 		String locationKey = network.get(0).getNodeId();
-		H2HTestData testData = new H2HTestData(data);
+		String contentKey = NetworkTestUtil.randomString();
+		H2HTestData testData = new H2HTestData(NetworkTestUtil.randomString());
+		
 		// manipulate the node, remove will not work
-		network.get(0).getConnection().getPeer().getPeerBean().storage(new FakeGetTestStorage());
+		network.get(0).getConnection().getPeer().getPeerBean().storage(new FakeGetTestStorage(testData));
 		// put some data to remove
 		network.get(0).getDataManager().putGlobal(locationKey, locationKey, testData).awaitUninterruptibly();
 
 		// initialize the process and the one and only step to test
 		Process process = new Process(network.get(0)) {
 		};
-		BaseRemoveProcessStep putStep = new BaseRemoveProcessStep(locationKey, contentKey, testData, null);
+		TestRemoveProcessStep putStep = new TestRemoveProcessStep(locationKey, contentKey, testData);
 		process.setNextStep(putStep);
 		TestProcessListener listener = new TestProcessListener();
 		process.addListener(listener);
@@ -94,35 +87,31 @@ public class RemoveProcessStepTest extends H2HJUnitTest {
 		} while (!listener.hasFailed());
 	}
 
-	@Override
-	@After
-	public void afterMethod() {
-		NetworkTestUtil.shutdownNetwork(network);
-		super.afterMethod();
-	}
-
 	@AfterClass
 	public static void cleanAfterClass() {
+		NetworkTestUtil.shutdownNetwork(network);
 		afterClass();
 	}
 
 	private class FakeGetTestStorage extends H2HStorageMemory {
+
+		private final H2HTestData data;
 		
-		public FakeGetTestStorage() {
+		public FakeGetTestStorage(H2HTestData data) {
 			super();
+			this.data = data;
 		}
 
 		@Override
 		public Data get(Number640 key) {
 			Data fakeData = null;
 			try {
-				fakeData = new Data(new H2HTestData(data));
+				fakeData = new Data(data);
 			} catch (IOException e) {
 				Assert.fail("Should not happen!");
 			}
 			return fakeData;
 		}
 	}
-	
-}
 
+}
