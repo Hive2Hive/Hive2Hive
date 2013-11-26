ffe289d910a4c9aface339bb92fcedd17194211b
Christian <Christian@89.206.88.95>
2013-11-26 11:23:33 +0100

    base user message class

---
+++ org.hive2hive.core/src/org/hive2hive/core/network/usermessages/UserMessage.java
@@ -0,0 +1,23 @@
+package org.hive2hive.core.network.usermessages;
+
+
+/**
+ * The base class of all UserMessage objects.
+ * 
+ * @author Christian
+ * 
+ */
+public abstract class UserMessage {
+
+	public void sign() {
+		
+	}
+
+	public void verify() {
+
+	}
+
+	public void execute() {
+		
+	}
+}
