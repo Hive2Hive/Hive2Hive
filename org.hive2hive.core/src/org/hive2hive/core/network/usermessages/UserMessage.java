7e235137f038396d2681cb7c74fabc55afd2ef42
Christian <Christian@89.206.88.95>
2013-11-26 12:11:04 +0100

    signature and verification of user message base type, made usermessage a runnable

--- org.hive2hive.core/src/org/hive2hive/core/network/usermessages/UserMessage.java
+++ org.hive2hive.core/src/org/hive2hive/core/network/usermessages/UserMessage.java
@@ -1,23 +1,74 @@
 package org.hive2hive.core.network.usermessages;
 
+import java.security.InvalidKeyException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SignatureException;
+
+import org.apache.log4j.Logger;
+import org.hive2hive.core.log.H2HLoggerFactory;
+import org.hive2hive.core.network.data.NetworkContent;
+import org.hive2hive.core.security.EncryptionUtil;
 
 /**
- * The base class of all UserMessage objects.
+ * The base class of all UserMessage objects, representing an encrypted and signed message that is stored on
+ * the receiving user's proxy.
  * 
  * @author Christian
  * 
  */
-public abstract class UserMessage {
+public abstract class UserMessage extends NetworkContent implements Runnable {
 
-	public void sign() {
-		
+	private final static Logger logger = H2HLoggerFactory.getLogger(UserMessage.class);
+	
+	private static final long serialVersionUID = -773794512479641000L;
+	
+	private byte[] objectSignState; // serialized state of this object when signed
+	private byte[] signature;
+
+	/**
+	 * Sign this user message such that the receiver can verify the sender with the senders public key.
+	 */
+	public final void sign(PrivateKey senderPrivateKey) {
+
+		byte[] signatureState = EncryptionUtil.serializeObject(this);
+		try {
+			signature = EncryptionUtil.sign(signatureState, senderPrivateKey);
+		} catch (InvalidKeyException | SignatureException e) {
+			logger.error("Exception while signing user message: ", e);
+		}
 	}
 
-	public void verify() {
-
+	/**
+	 * Verify this user message such that the sender can be uniquely identified.
+	 * @param senderPublicKey The public key of the assumed/accepted sender.
+	 */
+	public final boolean verify(PublicKey senderPublicKey) {
+		try {
+			return EncryptionUtil.verify(objectSignState, signature, senderPublicKey);
+		} catch (InvalidKeyException | SignatureException e) {
+			logger.error("Exception while verifying user message: ", e);
+		}
+		return false;
 	}
 
-	public void execute() {
-		
+	/**
+	 * Start the execution of this user message.
+	 */
+	public final void start() {
+		new Thread(this).start();
+	}
+
+	public final void run() {
+		execute();
+	}
+	
+	/**
+	 * The execution part of this user message.
+	 */
+	protected abstract void execute();
+	
+	public final byte[] getSignature() {
+		return signature;
 	}
 }
