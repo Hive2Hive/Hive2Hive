a7b980860425c30997fa96144e9f79c30a4892e9
Christian <Christian@ULTRA.home>
2013-11-27 21:39:28 +0100

    created login and exit menus

--- org.hive2hive.core/src/org/hive2hive/core/client/ConsoleMenu.java
+++ org.hive2hive.core/src/org/hive2hive/core/client/ConsoleMenu.java
@@ -1,10 +1,46 @@
 package org.hive2hive.core.client;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ConsoleMenu {
 
 	private final H2HConsole console;
+	private final ArrayList<ConsoleMenuItem> items;
 	
 	public ConsoleMenu(H2HConsole console) {
 		this.console = console;
+		this.items = new ArrayList<ConsoleMenuItem>();
+	}
+	
+	public boolean add(String displayText, IConsoleMenuCallback callback) {
+		return items.add(new ConsoleMenuItem(displayText, callback));
+	}
+	
+	public void show() {
+		
+		for (int i = 0; i < items.size(); ++i) {
+			ConsoleMenuItem item = items.get(i);
+			System.out.print(String.format("[%s] %s\n", i + 1, item.getDisplayText()));
+		}
+		System.out.println();
+		
+		Scanner input = new Scanner(System.in);
+		int chosen = 0;
+		try {
+			chosen = input.nextInt();
+		} catch (Exception e) {
+		}
+		
+		if (chosen > items.size() || chosen < 1){
+			System.out.println("Invalid option.\nPress enter to continue...");
+			input.nextLine();
+			input.nextLine();
+		} else {
+			ConsoleMenuItem item = items.get(chosen - 1);
+			IConsoleMenuCallback callback = item.getCallback();
+			callback.invoke();
+		}
+		input.close();
 	}
 }
