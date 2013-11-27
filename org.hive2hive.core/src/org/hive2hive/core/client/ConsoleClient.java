e4e106053102e497f28aafa0b40a2b1f6e201fff
Christian <Christian@ULTRA.home>
2013-11-27 11:08:31 +0100

    start of console client

---
+++ org.hive2hive.core/src/org/hive2hive/core/client/ConsoleClient.java
@@ -0,0 +1,17 @@
+package org.hive2hive.core.client;
+
+/**
+ * A dummy console-based client to use the Hive2Hive library.
+ * 
+ * @author Christian
+ * 
+ */
+public class ConsoleClient {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the Hive2Hive console client!");
+		
+		
+	}
+}
