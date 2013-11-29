042aa53d98a8c7816eb4f43a6340e0a3326d7b3e
Christian <Christian@JARVIS.home>
2013-11-29 14:52:33 +0100

    console menu flow refactoring

--- org.hive2hive.core/src/org/hive2hive/core/client/menu/H2HConsoleMenuItem.java
+++ org.hive2hive.core/src/org/hive2hive/core/client/menu/H2HConsoleMenuItem.java
@@ -2,46 +2,28 @@ package org.hive2hive.core.client.menu;
 
 public abstract class H2HConsoleMenuItem extends ConsoleMenuItem {
 
-	boolean hasException = false;
-	Exception catchedException = null;
-	
 	public H2HConsoleMenuItem(String displayText) {
 		super(displayText);
 	}
 
-	@Override
-	public void invoke() {
-		
-		hasException = false;
-		catchedException = null;
-		
-		initialize();
-		try {
-			execute();
-		} catch (Exception e){
-			hasException = true;
-			catchedException = e;
-		} finally {
-			end();
-		}
-	}
-	
 	@Override
 	protected void initialize() {
 		printSelection();
 	}
 
 	@Override
-	protected abstract void execute();
+	protected abstract void execute() throws Exception;
 
 	@Override
 	protected void end() {
-		if (hasException){
-			System.err.println(String.format("An exception has been thrown:\n %s\n", catchedException.getMessage()));
-		}
+		// do nothing as default
 	}
 
 	private void printSelection() {
-		System.out.println(String.format("Selected Option: %s", displayText));
+		System.out.println(String.format("Selected Option: %s\n", displayText));
+	}
+
+	protected void printSuccess() {
+		System.out.println(String.format("%s executed.\n", displayText));
 	}
 }
