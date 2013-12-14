1b22589546c46ed8bf76ac648be36b7607313d2d
Christian <Christian@ULTRA.home>
2013-12-14 15:27:35 +0100

    header adaption

--- org.hive2hive.client/src/org/hive2hive/client/ConsoleClient.java
+++ org.hive2hive.client/src/org/hive2hive/client/ConsoleClient.java
@@ -31,23 +31,23 @@ public class ConsoleClient {
 	}
 
 	private static void printHeader() {
-		System.out.println("\n**************************************************************************************************");
-		System.out.println("*                                                                           .´'`.                *");
-		System.out.println("*                                                                           |   |                *");
-		System.out.println("*                           Welcome to the Hive2Hive console client!      .´ `-´ `.              *");
-		System.out.println("*                                                                         |   |   |              *");
-		System.out.println("*                                                                          `-´ `-´               *");
-		System.out.println("**************************************************************************************************");
+		System.out.println("\n******************************************************************************************");
+		System.out.println("*                                                                        .´'`.             *");
+		System.out.println("*                                                                        |   |             *");
+		System.out.println("*                        Welcome to the Hive2Hive console client!      .´ `-´ `.           *");
+		System.out.println("*                                                                      |   |   |           *");
+		System.out.println("*                                                                       `-´ `-´            *");
+		System.out.println("********************************************************************************************");
 		System.out.println("\nConfigure your H2H network and nodes by browsing through the menus and follow the guides.\n");
 	}
 	
 	private static void printFooter() {
-		System.out.println("**************************************************************************************************");
-		System.out.println("*                                                                           .´'`.                *");
-		System.out.println("*                                                                           |   |                *");
-		System.out.println("*                                          Goodbye!                       .´ `-´ `.              *");
-		System.out.println("*                                    (www.hive2hive.com)                  |   |   |              *");
-		System.out.println("*                                                                          `-´ `-´               *");
-		System.out.println("**************************************************************************************************\n");
+		System.out.println("********************************************************************************************");
+		System.out.println("*                                                                        .´'`.             *");
+		System.out.println("*                                                                        |   |             *");
+		System.out.println("*                                          Goodbye!                    .´ `-´ `.           *");
+		System.out.println("*                                    (www.hive2hive.com)               |   |   |           *");
+		System.out.println("*                                                                       `-´ `-´            *");
+		System.out.println("********************************************************************************************\n");
 	}
 }
