d6270214f2c9b1547b9722ec64b85d74a02e6cf3
Ippes <seppi.gol@gmail.com>
2014-01-07 16:45:47 +0100

    futureGet modification for user profile task

--- org.hive2hive.core/src/org/hive2hive/core/network/data/futures/FutureGetListener.java
+++ org.hive2hive.core/src/org/hive2hive/core/network/data/futures/FutureGetListener.java
@@ -28,6 +28,8 @@ public class FutureGetListener implements BaseFutureListener<FutureGet> {
 	private final Number160 versionKey;
 	private final DataManager dataManager;
 
+	// flag for retries
+	private boolean retry = true;
 	// used to count get retries
 	private int getTries = 0;
 
@@ -39,6 +41,17 @@ public class FutureGetListener implements BaseFutureListener<FutureGet> {
 		this.versionKey = Number160.ZERO;
 		this.listener = listener;
 		this.dataManager = dataManager;
+	}
+
+	public FutureGetListener(Number160 locationKey, Number160 domainKey, DataManager dataManager,
+			IGetListener listener) {
+		this.locationKey = locationKey;
+		this.domainKey = domainKey;
+		this.contentKey = null;
+		this.versionKey = null;
+		this.listener = listener;
+		this.dataManager = dataManager;
+		this.retry = false;
 	}
 
 	public FutureGetListener(Number160 locationKey, Number160 domainKey, Number160 contentKey,
@@ -53,35 +66,53 @@ public class FutureGetListener implements BaseFutureListener<FutureGet> {
 
 	@Override
 	public void operationComplete(FutureGet future) throws Exception {
-		if (future == null || future.isFailed() || future.getData() == null) {
-			if (getTries++ < H2HConstants.GET_RETRIES) {
-				logger.warn(String
-						.format("Get retry #%s because future failed. location key = '%s' domain key = '%s' content key = '%s' version key = '%s'",
-								getTries, locationKey, domainKey, contentKey, versionKey));
-				if (versionKey == Number160.ZERO)
-					dataManager.get(locationKey, domainKey, contentKey).addListener(this);
-				else
-					dataManager.get(locationKey, domainKey, contentKey, versionKey).addListener(this);
+		if (retry) {
+			if (future == null || future.isFailed() || future.getData() == null) {
+				if (getTries++ < H2HConstants.GET_RETRIES) {
+					logger.warn(String
+							.format("Get retry #%s because future failed. location key = '%s' domain key = '%s' content key = '%s' version key = '%s'",
+									getTries, locationKey, domainKey, contentKey, versionKey));
+					if (versionKey == Number160.ZERO)
+						dataManager.get(locationKey, domainKey, contentKey).addListener(this);
+					else
+						dataManager.get(locationKey, domainKey, contentKey, versionKey).addListener(this);
+				} else {
+					logger.warn(String
+							.format("Get failed after %s tries. location key = '%s' domain key = '%s' content key = '%s' version key '%s'",
+									getTries, locationKey, domainKey, contentKey, versionKey));
+					if (listener != null)
+						listener.handleGetResult(null);
+				}
 			} else {
-				logger.warn(String
-						.format("Get failed after %s tries. location key = '%s' domain key = '%s' content key = '%s' version key '%s'",
-								getTries, locationKey, domainKey, contentKey, versionKey));
-				if (listener != null)
-					listener.handleGetResult(null);
+				NetworkContent content = (NetworkContent) future.getData().object();
+				if (content == null) {
+					logger.warn(String
+							.format("Get retry #%s because content is null. location key = '%s' domain key = '%s' content key = '%s' version key = '%s'",
+									getTries, locationKey, domainKey, contentKey, versionKey));
+					if (versionKey == Number160.ZERO)
+						dataManager.get(locationKey, domainKey, contentKey).addListener(this);
+					else
+						dataManager.get(locationKey, domainKey, contentKey, versionKey).addListener(this);
+				} else {
+					logger.debug(String
+							.format("got result = '%s' location key = '%s' domain key = '%s' content key = '%s' version key '%s'",
+									content.getClass().getSimpleName(), locationKey, domainKey, contentKey,
+									versionKey));
+					if (listener != null)
+						listener.handleGetResult(content);
+				}
 			}
 		} else {
-			NetworkContent content = (NetworkContent) future.getData().object();
-			if (content == null) {
+			if (future.getData() == null) {
 				logger.warn(String
-						.format("Get retry #%s because content is null. location key = '%s' domain key = '%s' content key = '%s' version key = '%s'",
-								getTries, locationKey, domainKey, contentKey, versionKey));
-				if (versionKey == Number160.ZERO)
-					dataManager.get(locationKey, domainKey, contentKey).addListener(this);
-				else
-					dataManager.get(locationKey, domainKey, contentKey, versionKey).addListener(this);
+						.format("Got null. location key = '%s' domain key = '%s' content key = '%s' version key = '%s'",
+								locationKey, domainKey, contentKey, versionKey));
+				if (listener != null)
+					listener.handleGetResult(null);
 			} else {
+				NetworkContent content = (NetworkContent) future.getData().object();
 				logger.debug(String
-						.format("got result= '%s' location key = '%s' domain key = '%s' content key = '%s' version key '%s'",
+						.format("got result = '%s' location key = '%s' domain key = '%s' content key = '%s' version key '%s'",
 								content.getClass().getSimpleName(), locationKey, domainKey, contentKey,
 								versionKey));
 				if (listener != null)
