a38d136f4168ada49c22dbdfff593e5bda4a14b6
Nico Rutishauser <nr@hotmail.ch>
2014-01-16 17:14:24 +0100

    Improve iterative get steps --> remove number of steps e.g. get fileChunk --> get all chunks in same step

--- org.hive2hive.core/src/org/hive2hive/core/process/download/GetFileChunkStep.java
+++ org.hive2hive.core/src/org/hive2hive/core/process/download/GetFileChunkStep.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
-import java.security.PrivateKey;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,78 +28,49 @@ public class GetFileChunkStep extends BaseGetProcessStep {
 	private final static Logger logger = H2HLoggerFactory.getLogger(GetFileChunkStep.class);
 
 	private final Path path;
-	private final List<KeyPair> chunksToGet;
+	private final List<KeyPair> chunkKeys;
 	private final List<Chunk> chunkBuffer;
 	private int currentChunkOrder;
-	private PrivateKey decryptionKey;
 
-	/**
-	 * Constructor for first step
-	 * 
-	 * @param destination
-	 * @param version
-	 */
 	public GetFileChunkStep(Path destination, FileVersion version) {
-		this(destination, 0, version.getChunkIds(), new ArrayList<Chunk>());
-		logger.debug("Start downloading '" + destination.toString() + "'");
-	}
-
-	/**
-	 * Constructor when multiple chunks are in the DHT that need to be download
-	 * 
-	 * @param file
-	 * @param currentChunkOrder
-	 * @param chunksToGet
-	 * @param chunkBuffer
-	 */
-	private GetFileChunkStep(Path path, int currentChunkOrder, List<KeyPair> chunksToGet,
-			List<Chunk> chunkBuffer) {
-		this.path = path;
-		this.currentChunkOrder = currentChunkOrder;
-		this.chunksToGet = chunksToGet;
-		this.chunkBuffer = chunkBuffer;
+		this.path = destination;
+		this.chunkKeys = version.getChunkIds();
+		this.currentChunkOrder = 0;
+		this.chunkBuffer = new ArrayList<Chunk>();
 	}
 
 	@Override
 	public void start() {
-		// download next chunk
-		KeyPair firstInList = chunksToGet.remove(0);
-		decryptionKey = firstInList.getPrivate(); // store current private key
-		logger.info("File " + path + ": " + chunksToGet.size() + " chunk(s) more to download.");
-		NetworkContent content = get(key2String(firstInList.getPublic()), H2HConstants.FILE_CHUNK);
-		evaluateResult(content);
-	}
-
-	public void evaluateResult(NetworkContent content) {
-		HybridEncryptedContent encrypted = (HybridEncryptedContent) content;
-		try {
-			NetworkContent decrypted = H2HEncryptionUtil.decryptHybrid(encrypted, decryptionKey);
-			chunkBuffer.add((Chunk) decrypted);
-			writeBufferToDisk();
-
-			if (chunksToGet.isEmpty()) {
-				// all chunks downloaded
-				if (chunkBuffer.isEmpty()) {
-					// normal case: done with the process.
-					logger.debug("Finished downloading file '" + path + "'.");
-					getProcess().setNextStep(null);
-				} else {
-					// should be empty
-					logger.error("All chunks downloaded but still some in buffer.");
-					getProcess().stop(
-							"Could not write all chunks to disk. We're stuck at chunk " + currentChunkOrder);
-				}
-			} else {
-				// more chunks to get. Continue with downloadint the next chunk
-				GetFileChunkStep nextStep = new GetFileChunkStep(path, currentChunkOrder, chunksToGet,
-						chunkBuffer);
-				getProcess().setNextStep(nextStep);
+		int counter = 0;
+		for (KeyPair chunkKey : chunkKeys) {
+			logger.info("File " + path + ": Downloading chunk " + counter++ + "/" + chunkKeys.size());
+			NetworkContent content = get(key2String(chunkKey.getPublic()), H2HConstants.FILE_CHUNK);
+			HybridEncryptedContent encrypted = (HybridEncryptedContent) content;
+			try {
+				NetworkContent decrypted = H2HEncryptionUtil.decryptHybrid(encrypted, chunkKey.getPrivate());
+				chunkBuffer.add((Chunk) decrypted);
+				writeBufferToDisk();
+			} catch (InvalidKeyException | DataLengthException | IllegalBlockSizeException
+					| BadPaddingException | IllegalStateException | InvalidCipherTextException
+					| IllegalArgumentException e) {
+				getProcess().stop("Could not decrypt file chunk. Reason: " + e.getMessage());
+				return;
+			} catch (IOException e) {
+				getProcess().stop("Could not write file chunk. Reason: " + e.getMessage());
+				return;
 			}
-		} catch (InvalidKeyException | DataLengthException | IllegalBlockSizeException | BadPaddingException
-				| IllegalStateException | InvalidCipherTextException | IllegalArgumentException e) {
-			getProcess().stop("Could not decrypt file chunk. Reason: " + e.getMessage());
-		} catch (IOException e) {
-			getProcess().stop("Could not write file chunk. Reason: " + e.getMessage());
+		}
+
+		// all chunks downloaded
+		if (chunkBuffer.isEmpty()) {
+			// normal case: done with the process.
+			logger.debug("Finished downloading file '" + path + "'.");
+			getProcess().setNextStep(null);
+		} else {
+			// should be empty
+			logger.error("All chunks downloaded but still some in buffer.");
+			getProcess()
+					.stop("Could not write all chunks to disk. We're stuck at chunk " + currentChunkOrder);
 		}
 	}
 
