20b6f1b6af50f220a147291b6e42af749f57f323
Nico Rutishauser <nr@hotmail.ch>
2014-01-17 20:31:22 +0100

    Get the public key all in the same step

--- org.hive2hive.core/src/org/hive2hive/core/process/notify/GetPublicKeysStep.java
+++ org.hive2hive.core/src/org/hive2hive/core/process/notify/GetPublicKeysStep.java
@@ -1,9 +1,7 @@
 package org.hive2hive.core.process.notify;
 
 import java.security.PublicKey;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,7 +15,7 @@ import org.hive2hive.core.network.data.NetworkContent;
 import org.hive2hive.core.process.common.get.BaseGetProcessStep;
 
 /**
- * Gets all public keys from these users iterativels
+ * Gets all public keys from these users iteratively
  * 
  * @author Nico
  * 
@@ -27,47 +25,25 @@ import org.hive2hive.core.process.common.get.BaseGetProcessStep;
 public class GetPublicKeysStep extends BaseGetProcessStep {
 
 	private final static Logger logger = H2HLoggerFactory.getLogger(GetPublicKeysStep.class);
-	private final List<String> users;
-	private final Map<String, PublicKey> keys;
-	private String current;
+	private final Set<String> users;
 
-	/**
-	 * Use this constructor for getting all the public keys
-	 * 
-	 * @param users
-	 */
 	public GetPublicKeysStep(Set<String> users) {
-		this(new ArrayList<String>(users), new HashMap<String, PublicKey>());
-		logger.debug("Start getting public keys from " + users.size() + " user(s)");
-	}
-
-	/**
-	 * Use this constructor when some keys are already existent (e.g. own key)
-	 */
-	public GetPublicKeysStep(List<String> moreToGet, Map<String, PublicKey> keys) {
-		this.users = moreToGet;
-		this.keys = keys;
+		this.users = users;
 	}
 
 	@Override
 	public void start() {
-		if (users.isEmpty()) {
-			// store the keys to the context
-			NotifyPeersProcessContext context = (NotifyPeersProcessContext) getProcess().getContext();
-			context.setUserPublicKeys(keys);
+		logger.debug("Start getting public keys from " + users.size() + " user(s)");
+		Map<String, PublicKey> keys = new HashMap<String, PublicKey>();
 
-			// continue with getting all locations
-			getProcess().setNextStep(new GetAllLocationsStep(keys.keySet()));
-		} else {
-			current = users.remove(0);
+		for (String user : users) {
 			boolean getRequired = true;
-
 			try {
 				// check if own user --> key is already in session
 				H2HSession session = getNetworkManager().getSession();
-				if (session.getCredentials().getUserId().equalsIgnoreCase(current)) {
+				if (session.getCredentials().getUserId().equalsIgnoreCase(user)) {
 					// current user is myself, the key is already present
-					keys.put(current, session.getKeyPair().getPublic());
+					keys.put(user, session.getKeyPair().getPublic());
 					getRequired = false;
 				}
 			} catch (NoSessionException e) {
@@ -76,21 +52,23 @@ public class GetPublicKeysStep extends BaseGetProcessStep {
 
 			if (getRequired) {
 				// needs to perform a get call
-				NetworkContent content = get(current, H2HConstants.USER_PUBLIC_KEY);
+				NetworkContent content = get(user, H2HConstants.USER_PUBLIC_KEY);
 				if (content == null) {
-					logger.error("Could not get the public key for user '" + current + "'. He get's ignored.");
+					logger.error("Could not get the public key for user '" + user + "'. He get's ignored.");
 				} else {
-					logger.debug("Got public key from user '" + current + "'. " + users.size()
+					logger.debug("Got public key from user '" + user + "'. " + users.size()
 							+ " keys more to get.");
 					UserPublicKey key = (UserPublicKey) content;
-					keys.put(current, key.getPublicKey());
+					keys.put(user, key.getPublicKey());
 				}
-
-				getProcess().setNextStep(new GetPublicKeysStep(users, keys));
-			} else {
-				// no get required --> go to next user
-				getProcess().setNextStep(new GetPublicKeysStep(users, keys));
 			}
 		}
+
+		// store the keys to the context
+		NotifyPeersProcessContext context = (NotifyPeersProcessContext) getProcess().getContext();
+		context.setUserPublicKeys(keys);
+
+		// continue with getting all locations
+		getProcess().setNextStep(new GetAllLocationsStep(keys.keySet()));
 	}
 }
