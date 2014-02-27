30da6b458a33607060db26ad6e80fea04ede63c0
Nico Rutishauser <nr@hotmail.ch>
2014-02-27 16:17:29 +0100

    Document the IFileManager (#54)
    Signed-off-by: Nico Rutishauser <nr@hotmail.ch>

--- org.hive2hive.core/src/org/hive2hive/core/api/interfaces/IFileConfiguration.java
+++ org.hive2hive.core/src/org/hive2hive/core/api/interfaces/IFileConfiguration.java
@@ -13,7 +13,7 @@ public interface IFileConfiguration {
 	/**
 	 * The maximum size that a file can have. The higher the size here, the heavier the network gets. More
 	 * data must be sent around. File size may be large if you only plan to use Hive2Hive in a (local)
-	 * High-speed network.<br/>
+	 * High-speed network.<br>
 	 * If you only need to synchronize small (meta) data, lower the file size here.
 	 * 
 	 * @return the number of bytes that a file can have
@@ -22,7 +22,7 @@ public interface IFileConfiguration {
 
 	/**
 	 * This parameter configures the number of available versions, once a file gets updated. In case the
-	 * developer does not want the version feature, just set this value to 1.
+	 * developer does not want the version feature, just set this value to 1.<br>
 	 * The more versions of a file are allowed, the heavier becomes the network.
 	 * 
 	 * @return the number of versions that a file may have
@@ -43,7 +43,7 @@ public interface IFileConfiguration {
 	/**
 	 * When uploading a file it gets chunked and distributed over the network. The network load becomes
 	 * statistically a more even distribution. Adding a large file to the network without chunking it forces
-	 * few peers to work very hard. We recommend to adapt the chunk size to the network performance.
+	 * few peers to work very hard. We recommend to adapt the chunk size to the network performance.<br>
 	 * Another aspect of chunking is that the file is more secure. An attacker that cracks (we don't hope he's
 	 * able to do so) one chunk may not see the whole file.
 	 * 
