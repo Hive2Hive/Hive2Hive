007439b27077c7038d5294352c9d2e93e3c3de9d
Nico Rutishauser <nr@hotmail.ch>
2014-06-28 15:36:11 +0200

    Issue #95
    refactored the verification.
    
    Signed-off-by: Nico Rutishauser <nr@hotmail.ch>

--- org.hive2hive.core/src/main/java/org/hive2hive/core/processes/implementations/notify/VerifyNotificationFactoryStep.java
+++ org.hive2hive.core/src/main/java/org/hive2hive/core/processes/implementations/notify/VerifyNotificationFactoryStep.java
@@ -1,6 +1,5 @@
 package org.hive2hive.core.processes.implementations.notify;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import org.hive2hive.core.processes.framework.abstracts.ProcessStep;
@@ -8,27 +7,30 @@ import org.hive2hive.core.processes.framework.exceptions.InvalidProcessStateExce
 import org.hive2hive.core.processes.framework.exceptions.ProcessExecutionException;
 import org.hive2hive.core.processes.implementations.context.interfaces.INotifyContext;
 
+/**
+ * Verifies whether the own user is in the notifications or not.
+ * 
+ * @author Nico
+ * 
+ */
 public class VerifyNotificationFactoryStep extends ProcessStep {
 
 	private final INotifyContext context;
+	// own User id
 	private final String userId;
 
 	public VerifyNotificationFactoryStep(INotifyContext context, String userId) {
 		this.context = context;
-		this.userId = userId; // own User id
+		this.userId = userId;
 	}
 
 	@Override
 	protected void doExecute() throws InvalidProcessStateException, ProcessExecutionException {
 		Set<String> usersToNotify = context.consumeUsersToNotify();
-		if (context.consumeMessageFactory().createUserProfileTask(userId) == null) {
-			// only private notification (or none)
-			usersToNotify = new HashSet<>(1);
-			if (context.consumeUsersToNotify().contains(userId))
-				usersToNotify.add(userId);
-			else
-				throw new ProcessExecutionException(
-						"Users can't be notified because the UserProfileTask is null and no notification of the own user.");
+		BaseNotificationMessageFactory messageFactory = context.consumeMessageFactory();
+		if (messageFactory.createUserProfileTask(userId) == null && !usersToNotify.contains(userId)) {
+			throw new ProcessExecutionException(
+					"Users can't be notified because the UserProfileTask is null and no notification of the own user.");
 		}
 	}
 
