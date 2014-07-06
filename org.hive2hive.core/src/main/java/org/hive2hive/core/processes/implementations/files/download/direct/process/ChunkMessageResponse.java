cf763a9d83fb97690fa524528cd6db75921cbd59
Nico Rutishauser <nr@hotmail.ch>
2014-07-06 12:32:15 +0200

    Add documentation + serial number to response message
    Signed-off-by: Nico Rutishauser <nr@hotmail.ch>

--- org.hive2hive.core/src/main/java/org/hive2hive/core/processes/implementations/files/download/direct/process/ChunkMessageResponse.java
+++ org.hive2hive.core/src/main/java/org/hive2hive/core/processes/implementations/files/download/direct/process/ChunkMessageResponse.java
@@ -4,7 +4,15 @@ import java.io.Serializable;
 
 import org.hive2hive.core.model.Chunk;
 
+/**
+ * The response from a client that was asked to deliver a certain chunk.
+ * 
+ * @author Nico
+ * 
+ */
 public class ChunkMessageResponse implements Serializable {
+
+	private static final long serialVersionUID = -7732972005871878370L;
 
 	enum AnswerType {
 		OK,
@@ -15,11 +23,21 @@ public class ChunkMessageResponse implements Serializable {
 	private final AnswerType answerType;
 	private final Chunk chunk;
 
+	/**
+	 * Everything is ok and the chunk can be returned
+	 * 
+	 * @param chunk the chunk that was requested
+	 */
 	public ChunkMessageResponse(Chunk chunk) {
 		this.chunk = chunk;
 		this.answerType = AnswerType.OK;
 	}
 
+	/**
+	 * The chunk cannot be returned for a certain reason.
+	 * 
+	 * @param answerType the type of error
+	 */
 	public ChunkMessageResponse(AnswerType answerType) {
 		this.chunk = null;
 		this.answerType = answerType;
