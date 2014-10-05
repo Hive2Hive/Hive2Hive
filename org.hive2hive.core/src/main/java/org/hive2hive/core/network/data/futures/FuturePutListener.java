507f893594da7dec291c8db69538a71ba4b940b4
Ippes <seppi.gol@gmail.com>
2014-10-05 14:48:09 +0200

    removed future confirm listener and integrated it into future put listener, avoiding deadlocks

--- org.hive2hive.core/src/main/java/org/hive2hive/core/network/data/futures/FuturePutListener.java
+++ org.hive2hive.core/src/main/java/org/hive2hive/core/network/data/futures/FuturePutListener.java
@@ -15,7 +15,7 @@ import net.tomp2p.peers.PeerAddress;
 import org.hive2hive.core.H2HConstants;
 import org.hive2hive.core.model.NetworkContent;
 import org.hive2hive.core.network.data.DataManager;
-import org.hive2hive.core.network.data.IDataManager.H2HPutStatus;
+import org.hive2hive.core.network.data.DataManager.H2HPutStatus;
 import org.hive2hive.core.network.data.parameters.IParameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -115,11 +115,84 @@ public class FuturePutListener extends BaseFutureAdapter<FuturePut> {
 			if (versionFork.isEmpty()) {
 				if (parameters.hasPrepareFlag()) {
 					// confirm put
-					status = dataManager.confirm(parameters);
+					dataManager.confirmUnblocked(parameters).addListener(new BaseFutureAdapter<FuturePut>() {
+
+						// used to count confirm retries
+						private int confirmTries = 0;
+
+						@Override
+						public void operationComplete(FuturePut future) throws Exception {
+							if (future.isFailed()) {
+								logger.warn("Confirm future was not successful. reason = '{}' {}", future.failedReason(),
+										parameters.toString());
+								retryConfirm();
+								return;
+							} else if (future.rawResult().isEmpty()) {
+								logger.warn("Returned raw results are empty. {}", parameters.toString());
+								retryConfirm();
+								return;
+							}
+
+							// analyze returned status
+							List<PeerAddress> fail = new ArrayList<PeerAddress>();
+							for (PeerAddress peeradress : future.rawResult().keySet()) {
+								Map<Number640, Byte> map = future.rawResult().get(peeradress);
+								if (map == null) {
+									logger.warn("A node gave no status (null) back. {}", parameters.toString());
+									fail.add(peeradress);
+								} else {
+									for (Number640 key : future.rawResult().get(peeradress).keySet()) {
+										byte status = future.rawResult().get(peeradress).get(key);
+										switch (PutStatus.values()[status]) {
+											case OK:
+												break;
+											case FAILED:
+											case FAILED_SECURITY:
+											case NOT_FOUND:
+												logger.warn("A node could not confirm data. reason = '{}'. {}",
+														PutStatus.values()[status], parameters.toString());
+												fail.add(peeradress);
+												break;
+											default:
+												logger.warn("Got an unknown status = '{}' {}", PutStatus.values()[status],
+														parameters.toString());
+										}
+									}
+								}
+							}
+
+							// check if majority of the contacted nodes responded with ok
+							if ((double) fail.size() < ((double) future.rawResult().size()) / 2.0) {
+								status = H2HPutStatus.OK;
+								latch.countDown();
+							} else {
+								logger.warn("{} of {} contacted nodes failed. {}", fail.size(), future.rawResult().size(),
+										parameters.toString());
+								retryConfirm();
+							}
+						}
+
+						/**
+						 * Retries a confirm till a certain threshold is reached (see
+						 * {@link H2HConstants.CONFIRM_RETRIES}). A {@link RetryPutListener} tries to confirm
+						 * again.
+						 */
+						private void retryConfirm() {
+							if (confirmTries++ < H2HConstants.CONFIRM_RETRIES) {
+								logger.warn("Confirm retry #{}. {}", confirmTries, parameters.toString());
+								// retry confirmation, attach itself as listener
+								dataManager.confirmUnblocked(parameters).addListener(this);
+							} else {
+								logger.error("Could not confirm put after {} tries. {}", confirmTries, parameters.toString());
+								status = H2HPutStatus.FAILED;
+								latch.countDown();
+							}
+						}
+					});
 				} else {
 					status = H2HPutStatus.OK;
+					latch.countDown();
 				}
-				latch.countDown();
 			} else {
 				logger.warn("Version fork after put detected. Rejecting put.");
 				// reject put
