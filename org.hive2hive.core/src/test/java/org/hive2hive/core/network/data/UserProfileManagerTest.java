d2f1166a5dc5f72e705bd80177d64324023ba5fe
Ippes <seppi.gol@gmail.com>
2014-10-06 23:08:29 +0200

    restructured user profile test, migrated user profile stress test to user profile tests

--- org.hive2hive.core/src/test/java/org/hive2hive/core/network/data/UserProfileManagerTest.java
+++ org.hive2hive.core/src/test/java/org/hive2hive/core/network/data/UserProfileManagerTest.java
@@ -1,18 +1,33 @@
 package org.hive2hive.core.network.data;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.KeyPair;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
+import org.apache.commons.io.FileUtils;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.hive2hive.core.H2HConstants;
 import org.hive2hive.core.H2HJUnitTest;
 import org.hive2hive.core.exceptions.GetFailedException;
 import org.hive2hive.core.exceptions.NoPeerConnectionException;
+import org.hive2hive.core.exceptions.NoSessionException;
 import org.hive2hive.core.exceptions.PutFailedException;
+import org.hive2hive.core.file.FileTestUtil;
+import org.hive2hive.core.model.FileIndex;
 import org.hive2hive.core.model.FolderIndex;
+import org.hive2hive.core.model.Index;
 import org.hive2hive.core.model.UserProfile;
 import org.hive2hive.core.network.NetworkManager;
 import org.hive2hive.core.network.NetworkTestUtil;
-import org.hive2hive.core.processes.util.UseCaseTestUtil;
+import org.hive2hive.core.network.data.parameters.IParameters;
+import org.hive2hive.core.network.data.parameters.Parameters;
+import org.hive2hive.core.security.EncryptedNetworkContent;
+import org.hive2hive.core.security.EncryptionUtil;
+import org.hive2hive.core.security.HashUtil;
 import org.hive2hive.core.security.UserCredentials;
 import org.hive2hive.processframework.abstracts.ProcessStep;
 import org.hive2hive.processframework.decorators.AsyncComponent;
@@ -21,7 +36,6 @@ import org.hive2hive.processframework.exceptions.ProcessExecutionException;
 import org.hive2hive.processframework.interfaces.IProcessComponent;
 import org.hive2hive.processframework.util.H2HWaiter;
 import org.hive2hive.processframework.util.TestProcessComponentListener;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -31,13 +45,16 @@ import org.junit.Test;
  * Test the {@link UserProfileManager} which handles concurrency when editing a user profile with multiple
  * processes. Many combinations of get, put and modify are tested.
  * 
- * @author Nico
- * 
+ * @author Nico, Seppi
  */
 public class UserProfileManagerTest extends H2HJUnitTest {
 
+	private static ArrayList<NetworkManager> network;
+	private static final int networkSize = 10;
+
 	private UserCredentials userCredentials;
 	private NetworkManager client;
+	private File root;
 
 	private enum Operation {
 		PUT,
@@ -49,13 +66,34 @@ public class UserProfileManagerTest extends H2HJUnitTest {
 	public static void initTest() throws Exception {
 		testClass = UserProfileManagerTest.class;
 		beforeClass();
+		network = NetworkTestUtil.createNetwork(networkSize);
+	}
+
+	@AfterClass
+	public static void cleanAfterClass() {
+		NetworkTestUtil.shutdownNetwork(network);
+		afterClass();
 	}
 
 	@Before
-	public void setup() throws NoPeerConnectionException {
+	public void setup() throws NoPeerConnectionException, InvalidCipherTextException, IOException {
 		userCredentials = NetworkTestUtil.generateRandomCredentials();
-		client = NetworkTestUtil.createNetwork(1).get(0);
-		UseCaseTestUtil.register(userCredentials, client);
+		client = NetworkTestUtil.getRandomNode(network);
+		root = FileTestUtil.getTempDirectory();
+
+		// create an user profile
+		UserProfile userProfile = new UserProfile(userCredentials.getUserId());
+		// encrypt it (fake encryption)
+		EncryptedNetworkContent encrypted = client.getDataManager().getEncryption().encryptAES(userProfile, null);
+		encrypted.setVersionKey(userProfile.getVersionKey());
+		encrypted.generateVersionKey();
+
+		// upload user profile, avoids register step
+		IParameters parameters = new Parameters().setLocationKey(userCredentials.getProfileLocationKey())
+				.setContentKey(H2HConstants.USER_PROFILE).setVersionKey(encrypted.getVersionKey())
+				.setNetworkContent(encrypted).setProtectionKeys(userProfile.getProtectionKeys())
+				.setTTL(userProfile.getTimeToLive());
+		client.getDataManager().put(parameters);
 	}
 
 	@Test
@@ -98,9 +136,6 @@ public class UserProfileManagerTest extends H2HJUnitTest {
 	/**
 	 * Transforms the operations into a set of processes and starts them all. The processes are started with a
 	 * small delay, but in the same order as the parameters. The method blocks until all processes are done.
-	 * 
-	 * @throws InvalidProcessStateException
-	 * @throws NoPeerConnectionException
 	 */
 	private void executeProcesses(Operation... operations) throws GetFailedException, InterruptedException,
 			InvalidProcessStateException, NoPeerConnectionException {
@@ -137,33 +172,16 @@ public class UserProfileManagerTest extends H2HJUnitTest {
 		} while (!allFinished);
 	}
 
-	@After
-	public void shutdown() {
-		client.disconnect();
-	}
-
-	@AfterClass
-	public static void cleanAfterClass() {
-		afterClass();
-	}
-
 	/**
 	 * Gets the user profile using the {@link UserProfileManager}.
 	 * 
 	 * @author Nico
-	 * 
 	 */
 	private class TestUserProfileStep extends ProcessStep {
 
 		private final UserProfileManager profileManager;
 		private final Operation operation;
 
-		/**
-		 * 
-		 * @param profileManager
-		 * @param put if true, it performs a put operation
-		 * @param modify if true, it does a modification
-		 */
 		public TestUserProfileStep(UserProfileManager profileManager, Operation operation) {
 			this.profileManager = profileManager;
 			this.operation = operation;
@@ -186,5 +204,53 @@ public class UserProfileManagerTest extends H2HJUnitTest {
 				throw new ProcessExecutionException(e);
 			}
 		}
+	}
+
+	@Test
+	public void testStress() throws NoSessionException, GetFailedException, PutFailedException, IOException,
+			NoPeerConnectionException {
+		UserProfileManager profileManager = new UserProfileManager(client.getDataManager(), userCredentials);
+		Random random = new Random();
+
+		for (int i = 0; i < 10; i++) {
+			boolean isFolder = random.nextBoolean();
+
+			File file = null;
+			KeyPair keys = null;
+			byte[] md5Hash = null;
+			if (!isFolder) {
+				file = new File(root, NetworkTestUtil.randomString());
+				FileUtils.writeStringToFile(file, NetworkTestUtil.randomString());
+				keys = EncryptionUtil.generateRSAKeyPair(H2HConstants.KEYLENGTH_META_FILE);
+				md5Hash = HashUtil.hash(file);
+			}
+
+			String pid = UUID.randomUUID().toString();
+			UserProfile profile = profileManager.getUserProfile(pid, true);
+
+			List<FolderIndex> indexes = getIndexList(profile.getRoot());
+
+			if (isFolder) {
+				new FolderIndex(indexes.get(random.nextInt(indexes.size())), null, NetworkTestUtil.randomString());
+			} else {
+				new FileIndex(indexes.get(random.nextInt(indexes.size())), keys, file.getName(), md5Hash);
+			}
+
+			profileManager.readyToPut(profile, pid);
+		}
+	}
+
+	public static List<FolderIndex> getIndexList(Index node) {
+		List<FolderIndex> digest = new ArrayList<FolderIndex>();
+		if (node.isFolder()) {
+			// add self
+			digest.add((FolderIndex) node);
+			// add children
+			for (Index child : ((FolderIndex) node).getChildren()) {
+				if (child.isFolder())
+					digest.addAll(getIndexList(child));
+			}
+		}
+		return digest;
 	}
 }
