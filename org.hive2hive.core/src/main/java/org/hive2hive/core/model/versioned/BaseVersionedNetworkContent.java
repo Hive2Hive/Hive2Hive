bfced939d7e829f2f73b6381f53576bccc9df95e
Ippes <seppi.gol@gmail.com>
2014-10-07 11:50:20 +0200

    belongs to last commit

---
+++ org.hive2hive.core/src/main/java/org/hive2hive/core/model/versioned/BaseVersionedNetworkContent.java
@@ -0,0 +1,59 @@
+package org.hive2hive.core.model.versioned;
+
+import java.io.IOException;
+
+import org.hive2hive.core.model.BaseNetworkContent;
+
+import net.tomp2p.peers.Number160;
+
+/**
+ * All data of <code>Hive2Hive</code> which has to be stored in the DHT and are frequently manipulated are
+ * using this wrapper.
+ * 
+ * @author Seppi
+ */
+public abstract class BaseVersionedNetworkContent extends BaseNetworkContent {
+
+	private static final long serialVersionUID = 8206000167141687813L;
+
+	/**
+	 * Some data has a version key (used to differentiate versions). Default value.
+	 */
+	private Number160 versionKey = Number160.ZERO;
+
+	/**
+	 * Some data is based on other data. Default value.
+	 */
+	private Number160 basedOnKey = Number160.ZERO;
+
+	public Number160 getVersionKey() {
+		return versionKey;
+	}
+
+	public void setVersionKey(Number160 versionKey) {
+		this.versionKey = versionKey;
+	}
+
+	public Number160 getBasedOnKey() {
+		return basedOnKey;
+	}
+
+	public void setBasedOnKey(Number160 basedOnKey) {
+		this.basedOnKey = basedOnKey;
+	}
+
+	/**
+	 * Call this method in front of a put into the network if the data is a new version or will have other
+	 * versions.
+	 * 
+	 * @throws IOException
+	 */
+	public void generateVersionKey() throws IOException {
+		// re-attach version keys
+		basedOnKey = versionKey;
+		// increase counter
+		long counter = basedOnKey.timestamp() + 1;
+		// create new version key based on increased counter and hash
+		versionKey = new Number160(counter, new Number160(this.hashCode()).number96());
+	}
+}
