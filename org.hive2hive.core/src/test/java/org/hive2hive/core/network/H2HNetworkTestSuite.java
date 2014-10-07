408ccc7ebb9bcbe68e4245f58e03431790ff32b1
Ippes <seppi.gol@gmail.com>
2014-10-07 08:32:29 +0200

    adapted H2HNetworkTestSuite, all tests in network package are stable

--- org.hive2hive.core/src/test/java/org/hive2hive/core/network/H2HNetworkTestSuite.java
+++ org.hive2hive.core/src/test/java/org/hive2hive/core/network/H2HNetworkTestSuite.java
@@ -1,7 +1,13 @@
 package org.hive2hive.core.network;
 
+import org.hive2hive.core.network.data.ContentProtectionTest;
 import org.hive2hive.core.network.data.DataManagerTest;
+import org.hive2hive.core.network.data.PublicKeyManagerTest;
 import org.hive2hive.core.network.data.UserProfileManagerTest;
+import org.hive2hive.core.network.data.VersionManagerTest;
+import org.hive2hive.core.network.data.futures.FutureGetTest;
+import org.hive2hive.core.network.data.futures.FuturePutTest;
+import org.hive2hive.core.network.data.futures.FutureRemoveTest;
 import org.hive2hive.core.network.messages.BaseMessageTest;
 import org.hive2hive.core.network.messages.BaseRequestMessageTest;
 import org.hive2hive.core.network.messages.MessageSignatureTest;
@@ -20,12 +26,14 @@ import org.junit.runners.Suite.SuiteClasses;
 		// Network
 		ConnectionTest.class,
 		// Network, Data
-		DataManagerTest.class, UserProfileManagerTest.class,
+		DataManagerTest.class, UserProfileManagerTest.class, ContentProtectionTest.class, PublicKeyManagerTest.class,
+		VersionManagerTest.class,
+		// Network, Data, Futures
+		FutureGetTest.class, FuturePutTest.class, FutureRemoveTest.class,
 		// Network, Message
 		MessageSignatureTest.class, BaseMessageTest.class, BaseRequestMessageTest.class,
 		// Network, Message, Direct
-		BaseDirectRequestMessageTest.class,
-})
+		BaseDirectRequestMessageTest.class, })
 public class H2HNetworkTestSuite {
 
 }
