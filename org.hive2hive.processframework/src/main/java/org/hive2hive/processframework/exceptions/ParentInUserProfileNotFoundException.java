582e2c523ffd26667a435ce01fe841a1ce303f43
raute <claudioanliker@hotmail.com>
2014-10-22 18:33:51 +0200

    + ParentInUserProfileNotFoundException

---
+++ org.hive2hive.processframework/src/main/java/org/hive2hive/processframework/exceptions/ParentInUserProfileNotFoundException.java
@@ -0,0 +1,9 @@
+package org.hive2hive.processframework.exceptions;
+
+public class ParentInUserProfileNotFoundException extends ProcessExecutionException{
+
+	public ParentInUserProfileNotFoundException(Exception cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+}
