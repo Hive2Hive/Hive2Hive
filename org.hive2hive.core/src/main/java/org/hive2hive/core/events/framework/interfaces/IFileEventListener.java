0967f9ee35433474350521c83b2ea568fe3b72ac
Ippes <seppi.gol@gmail.com>
2014-10-29 20:59:18 +0100

    added IFileUpdateEvent

--- org.hive2hive.core/src/main/java/org/hive2hive/core/events/framework/interfaces/IFileEventListener.java
+++ org.hive2hive.core/src/main/java/org/hive2hive/core/events/framework/interfaces/IFileEventListener.java
@@ -5,11 +5,15 @@ import net.engio.mbassy.listener.Handler;
 import org.hive2hive.core.events.framework.interfaces.file.IFileAddEvent;
 import org.hive2hive.core.events.framework.interfaces.file.IFileDeleteEvent;
 import org.hive2hive.core.events.framework.interfaces.file.IFileMoveEvent;
+import org.hive2hive.core.events.framework.interfaces.file.IFileUpdateEvent;
 
 public interface IFileEventListener {
 
 	@Handler
 	void onFileAdd(IFileAddEvent fileEvent);
+
+	@Handler
+	void onFileUpdate(IFileUpdateEvent fileEvent);
 
 	@Handler
 	void onFileDelete(IFileDeleteEvent fileEvent);
