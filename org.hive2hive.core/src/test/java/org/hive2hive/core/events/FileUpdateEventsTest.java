f674836bbf32ff3f0def97b2751c0634e203f721
Ippes <seppi.gol@gmail.com>
2014-11-03 18:37:19 +0100

    stub for file update event test

---
+++ org.hive2hive.core/src/test/java/org/hive2hive/core/events/FileUpdateEventsTest.java
@@ -0,0 +1,22 @@
+package org.hive2hive.core.events;
+
+import java.io.IOException;
+
+import org.hive2hive.core.exceptions.NoPeerConnectionException;
+import org.hive2hive.core.exceptions.NoSessionException;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class FileUpdateEventsTest extends FileEventsTest {
+
+	static {
+		testClass = FileUpdateEventsTest.class;
+	}
+
+	@Test
+	@Ignore
+	public void testFileUpdateEvent() throws NoPeerConnectionException, IOException, NoSessionException {
+
+	}
+
+}
