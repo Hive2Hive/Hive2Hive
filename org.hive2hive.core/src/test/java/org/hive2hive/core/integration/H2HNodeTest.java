2b76610988796ba3dfd235932483b24f7abb98e5
nicoruti <nico.rutishauser@diligent-it.com>
2014-11-16 21:04:49 +0100

    Remove invalid test (file tree)

--- org.hive2hive.core/src/test/java/org/hive2hive/core/integration/H2HNodeTest.java
+++ org.hive2hive.core/src/test/java/org/hive2hive/core/integration/H2HNodeTest.java
@@ -16,7 +16,6 @@ import org.hive2hive.core.api.interfaces.INetworkConfiguration;
 import org.hive2hive.core.exceptions.IllegalFileLocation;
 import org.hive2hive.core.exceptions.NoPeerConnectionException;
 import org.hive2hive.core.exceptions.NoSessionException;
-import org.hive2hive.core.file.IFileAgent;
 import org.hive2hive.core.security.UserCredentials;
 import org.hive2hive.core.utils.FileTestUtil;
 import org.hive2hive.core.utils.NetworkTestUtil;
@@ -93,50 +92,6 @@ public class H2HNodeTest extends H2HJUnitTest {
 		FileUtils.write(testFile, "Hello World");
 
 		loggedInNode.getFileManager().add(testFile);
-	}
-
-	@Test
-	public void testAddFileTree() throws IOException, IllegalFileLocation, NoSessionException, NoSuchFieldException,
-			SecurityException, IllegalArgumentException, IllegalAccessException, NoPeerConnectionException {
-		// /folder1/
-		// /folder1/test1.txt
-		// /folder1/folder2/test2.txt
-		File folder1 = new File(fileAgent.getRoot(), "folder1");
-		folder1.mkdir();
-		File test1File = new File(folder1, "test1.txt");
-		FileUtils.write(test1File, "Hello World 1");
-
-		File folder2 = new File(folder1, "folder2");
-		folder2.mkdir();
-		File test2File = new File(folder2, "test2.txt");
-		FileUtils.write(test2File, "Hello World 2");
-
-		IProcessComponent<?> process = loggedInNode.getFileManager().add(folder1);
-		TestExecutionUtil.executeProcessTillSucceded(process);
-
-		// TODO wait for all async process to upload
-
-		// then start 2nd client and login
-		IFileAgent fileAgentUser2 = new TestFileAgent();
-		IH2HNode newNode = network.get((random.nextInt(NETWORK_SIZE / 2) + NETWORK_SIZE / 2));
-
-		IProcessComponent<?> loginProcess = newNode.getUserManager().login(credentials, fileAgentUser2);
-		TestExecutionUtil.executeProcessTillSucceded(loginProcess);
-
-		// TODO wait for login process to download all files
-
-		// verfiy that all files are here
-		folder1 = new File(fileAgentUser2.getRoot(), "folder1");
-		Assert.assertTrue(folder1.exists());
-
-		test1File = new File(folder1, "test1.txt");
-		Assert.assertEquals("Hello World 1", FileUtils.readFileToString(test1File));
-
-		folder2 = new File(folder1, "folder2");
-		Assert.assertTrue(folder2.exists());
-
-		test2File = new File(folder2, "test2.txt");
-		Assert.assertEquals("Hello World 2", FileUtils.readFileToString(test2File));
 	}
 
 	@Test
