cfc4e7ec5cfca16aff889efa9a0ca47b1a088888
nicoruti <nico.rutishauser@diligent-it.com>
2014-12-05 08:43:20 +0100

    Add helper method to visit all nodes

--- org.hive2hive.core/src/main/java/org/hive2hive/core/processes/files/list/FileNode.java
+++ org.hive2hive.core/src/main/java/org/hive2hive/core/processes/files/list/FileNode.java
@@ -1,7 +1,9 @@
 package org.hive2hive.core.processes.files.list;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.hive2hive.core.model.UserPermission;
@@ -135,5 +137,33 @@ public class FileNode {
 	public String toString() {
 		return String.format("%s: %s [%s] %s", isFile() ? "File" : "Folder", getPath(), getUserPermissions(),
 				isFile() ? String.format("(MD5: %s)", EncryptionUtil.byteToHex(getMd5())) : "");
+	}
+
+	/**
+	 * Returns a list of all nodes in preorder. One can specify whether only files, folders or both is desired
+	 */
+	public static List<FileNode> getNodeList(FileNode root, boolean addFiles, boolean addFolders) {
+		if (!addFiles && !addFolders) {
+			throw new IllegalArgumentException("Must visit either files, folders or both");
+		}
+		List<FileNode> list = new ArrayList<FileNode>();
+		preorder(root, list, addFiles, addFolders);
+		return list;
+	}
+
+	private static void preorder(FileNode current, List<FileNode> list, boolean addFiles, boolean addFolders) {
+		if (current == null) {
+			return;
+		}
+
+		if (current.isFile() && addFiles || current.isFolder() && addFolders) {
+			list.add(current);
+		}
+
+		if (current.isFolder()) {
+			for (FileNode child : current.getChildren()) {
+				preorder(child, list, addFiles, addFolders);
+			}
+		}
 	}
 }
