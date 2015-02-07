b7b2fd1a2219f08a0207b33d2d8767dfc63e9bab
nicoruti <nico.rutishauser@diligent-it.com>
2015-02-07 17:53:30 +0100

    Add documentation to ISecurityClassProvider

--- org.hive2hive.core/src/main/java/org/hive2hive/core/serializer/ISecurityClassProvider.java
+++ org.hive2hive.core/src/main/java/org/hive2hive/core/serializer/ISecurityClassProvider.java
@@ -6,7 +6,13 @@ import java.security.interfaces.RSAPublicKey;
 
 /**
  * Provides classes for the respective security provider to serialize / deserialize using
- * {@link FSTSerializer}.
+ * {@link FSTSerializer}. The problem is that different security providers use different key implementations.
+ * BouncyCastle uses for example "org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPublicKey" whereas
+ * SpongyCastle uses "org.spongycastle.jcajce.provider.asymmetric.rsa.BCRSAPublicKey" and default Android
+ * security providers uses "com.android.org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPublicKey". These
+ * class names are by default serialized and sent over the network. Recipients with other security providers
+ * cannot find the class by their names during decoding. Therefore, just indexes are sent over the network and
+ * mapped back to the key implementation stated through this implementation.
  * 
  * @author Nico
  *
